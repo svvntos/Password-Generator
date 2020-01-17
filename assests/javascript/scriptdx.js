7345e31e9eee4d9b860fafabf7650718055014f3
svvntos <joesantos123@live.com>
2020-01-17 18:01:43 -0500

    Updating javascript

--- assests/javascript/scriptdx.js
+++ assests/javascript/scriptdx.js
@@ -1,73 +1,67 @@
-var generateBtn = document.querySelector("#generate");
-var copyBtn = document.querySelector("#copy");
-var passwordText = document.querySelector("#password");
+var upperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+var lowerCase = ['abcdefhijklmnopqrstuvwxyz'];
+var numbers = ['1234567890']
+var symbols = ['!@#$%&?/~+;-'];
 
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-var symbols = ['!', '@', '#', '$', '%', '&', '?', '/', '~', '+', ';', '-'];
+document.getElementById("generate").addEventListener("click", function () {
 
-var randomPassword = [];
-// Write password to the #password input
+    function generatePassword() {
+        var retVal = "";
+        numberOfCharacters = "";
+        var passwordLength = prompt("How many characters would you like to use?");
 
+        if (passwordLength < 8 || passwordLength > 128) {
+            alert("Please put a min of 8 and max of 128");
+            return generatePassword();
+        }
 
+        var upperCaseQuestion = confirm("Would you like to use Uppercase letters? Click ok for yes, or cancel for no.");
+        if (upperCaseQuestion) {
+            numberOfCharacters = numberOfCharacters + upperCase
+        }
+        var lowerCaseQuestion = confirm("Would you like to use Lowercase letters? Click ok for yes, or cancel for no.");
+        if (lowerCaseQuestion) {
+            numberOfCharacters = numberOfCharacters + lowerCase
+        }
+        var numbersQuestion = confirm("Would you like to use Numbers? Click ok for yes, or cancel for no.");
+        if (numbersQuestion) {
+            numberOfCharacters = numberOfCharacters + numbers
+        }
+        var symbolsQuestion = confirm("Would you like to use Symbols? Click ok for yes, or cancel for no.");
+        if (symbolsQuestion) {
+            numberOfCharacters = numberOfCharacters + symbols
+        }
+        if (!upperCaseQuestion && !lowerCaseQuestion && !numbersQuestion && !symbolsQuestion) {
+            alert("Pick a vaild character.")
+            return generatePassword();
+        }
+        console.log(passwordLength);
+        console.log(upperCaseQuestion);
+        console.log(lowerCaseQuestion);
+        console.log(numbersQuestion);
+        console.log(symbolsQuestion);
 
-
-
-
-function generatePassword(upperCase, lowerCase, number, symbols) {
-    var theArray = arr.flat(); 
-    var newPassword = [];
-    for (var i = 0; i < number; i++) {
-        var randomIndex = Math.floor(Math.random() * theArray.length - 1) + 1;
-        newPassword.push(theArray[randomIndex])
+        for (var i = 0; i < passwordLength; ++i) {
+            retVal += (numberOfCharacters.charAt(Math.floor(Math.random() * numberOfCharacters.length)));
+        }
+        return retVal;
     }
-    return newPassword.join('');    
-
-}
-
-function writePassword() {
-
-    var numberOfCharacters = parseInt(prompt("How many characters would you like to use?"));
-
-    if (numberOfCharacters < 8 || numberOfCharacters > 15) {
-        alert("Please put a min of 8 and max of 15");
-        return;
-    } else if (isNaN(numberOfCharacters)) {
-        alert("Please input a vaild charaters!");
-        return;
-    }
+    numberOfCharacters = "";
+    var retVal = generatePassword();
+    console.log(retVal);
+    document.getElementById("password").innerHTML = retVal;
+})
+function myFunction() {
+  
+    var copyText = document.getElementById("password");
     
-    var upperCaseQuestion = confirm("Would you like to use Uppercase letters? Click ok for yes, or cancel for no.");
-    var lowerCaseQuestion = confirm("Would you like to use Lowercase letters? Click ok for yes, or cancel for no.");
-    var numbersQuestion   = confirm("Would you like to use Numbers? Click ok for yes, or cancel for no.");
-    var symbolsQuestion   = confirm("Would you like to use Symbols? Click ok for yes, or cancel for no.");
-
-    if (upperCaseQuestion) {
-        randomPassword.push(upperCase)
-    }
-    if (lowerCaseQuestion) {
-        randomPassword.push(lowerCase);
-    }
-    if (numbersQuestion)  {
-        randomPassword.push(numbers);
-    }
-    if (symbolsQuestion)  {
-        randomPassword.push(symbols);
-    } 
-    else if (!upperCaseQuestion && !lowerCaseQuestion && !numbersQuestion && !symbolsQuestion){
-        alert("Pick a vaild character.")
-        return ;
-    }
-   var password = generatePassword(randomPassword, numberOfCharacters);
-   passwordText.value = password;
-}
-
-
-    function copyToClipboard() {
-        // BONUS 
-    }
-
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
-    copyBtn.addEventListener("click", copyToClipboard);
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    
+    document.execCommand("copy");
+    
+    alert("Copied the text: " + copyText.value);
+  }
+document.getElementById("copytext").addEventListener("click", function(){
+    copyText();
+})
